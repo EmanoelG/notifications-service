68557b0e3019ba05e6aadc5edcb351352522f5e6
EmanoelG <emanoelgalvao42@gmail.com>
2022-12-13 21:12:02 -0300

    Injecao e inversao de dependencia...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MailService } from './mail/mail.service';
+import { PostMarkMailService } from './mail/postmark-mail.service';
 import { SMTPMAILservice } from './mail/smpt-mail.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [
-    {
-      provide: MailService,
-      useClass: SMTPMAILservice,
-    }
+  {
+    provide: MailService,
+    useClass: PostMarkMailService,
+  }
 
   ],
 })
